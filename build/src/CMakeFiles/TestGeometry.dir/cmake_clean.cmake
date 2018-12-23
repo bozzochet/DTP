6bee041a322c9d9abcfc9ed2b80fe59e4b21a4b5
Francesco Faldi <faldifrancesco@libero.it>
2018-12-23 15:14:10 +0100

    ho aggiunto i piani di silicio e il calorimetro

---
+++ build/src/CMakeFiles/TestGeometry.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TestGeometry.dir/DetectorConstruction.o"
+  "CMakeFiles/TestGeometry.dir/DetectorMessenger.o"
+  "libTestGeometry.pdb"
+  "libTestGeometry.so.1"
+  "libTestGeometry.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TestGeometry.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
