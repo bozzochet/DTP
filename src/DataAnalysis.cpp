3bf0aa1614383c803a065b9205cf875458a1b513
Francesco Faldi <faldifrancesco@libero.it>
2019-01-27 14:04:09 +0100

    in DataAnalysis i cluster vengono inseriti in un vector di vector ma da segmentation violation

--- src/DataAnalysis.cpp
+++ src/DataAnalysis.cpp
@@ -9,11 +9,13 @@
 #include<vector>
 #include"TClonesArray.h"
 #include<iostream>
+#include"TTreeReader.h"
 using namespace std;
 
 int main(int argc,char** argv){
 
 auto inFile = TFile::Open(argv[1]);
+
 TTree *events;
 inFile->GetObject("Tree", events);
 events->Print();
@@ -21,18 +23,30 @@ TClonesArray *a = new TClonesArray("TrCluster", 200);
 events->SetBranchAddress("Events", &a);
 
 TCanvas* c=new TCanvas();
-TH1F* h=new TH1F("h","h",100,-0.5,0.5);
+TH1F* h=new TH1F("h","h",100,0,1000);
+
+vector< vector<TrCluster> > v(events->GetEntries(), vector<TrCluster>());
 
 for (int i=0; i< events->GetEntries(); i++) {
 	events->GetEntry(i);
 	for (int j=0; j< a->GetEntries(); j++){
 		TrCluster *cl = (TrCluster*)a->At(j);
-		h->Fill(cl->segm);
+		v[cl->layer].push_back(*cl);
 	}
 }
 
-h->Draw();
-c->SaveAs("test.pdf");
+int tot=0;
+
+for (int i=0; i< events->GetEntries(); i++) {
+	for (int j=0; j< v[i].size(); j++){
+		tot++;
+	}
+}
+
+cout<<tot<<endl;
+
+//h->Draw();
+//c->SaveAs("test.pdf");
 }
 
 
