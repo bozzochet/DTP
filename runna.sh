cc8739e3cafc8e75f55d88a51d9a0248e7482738
userlucio <lucio.moriconi@outlook.com>
2020-05-08 16:57:43 +0200

    runna.sh moved to .so

--- runna.sh
+++ runna.sh
@@ -1,4 +1,4 @@
 echo "LD_LIBRARY_PATH=$LD_LIBRARY_PATH"
 
-GGSPenny -g src/libTestGeometry.dylib -d run.mac -ro test.root
-GGSPenny -g src/libTestGeometry_nocalo.dylib -d run.mac -ro test_nocalo.root
+GGSPenny -g src/libTestGeometry.so -d run.mac -ro test.root
+GGSPenny -g src/libTestGeometry_nocalo.so -d run.mac -ro test_nocalo.root
