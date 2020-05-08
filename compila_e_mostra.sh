a4d75318a846184e2bfe8db1bcc490a7b211fc55
Matteo Duranti <bozzo1983@gmail.com>
2020-05-08 16:50:07 +0200

    changed .dylib to .so in compila_e_mostra.sh

--- compila_e_mostra.sh
+++ compila_e_mostra.sh
@@ -3,8 +3,8 @@ echo "LD_LIBRARY_PATH=$LD_LIBRARY_PATH"
 cmake ${PWD/%_build} -DGGS_DIR=$GGS_SYS/
 make
 rm -v src/TestGeometry.gdml
-GGSWolowitz -g src/libTestGeometry.dylib -o src/TestGeometry.gdml -t gdml
+GGSWolowitz -g src/libTestGeometry.so -o src/TestGeometry.gdml -t gdml
 #GGSLeonard -G src/TestGeometry.gdml
 rm -v src/TestGeometry_nocalo.gdml
-GGSWolowitz -g src/libTestGeometry_nocalo.dylib -o src/TestGeometry_nocalo.gdml -t gdml
+GGSWolowitz -g src/libTestGeometry_nocalo.so -o src/TestGeometry_nocalo.gdml -t gdml
 #GGSLeonard -G src/TestGeometry_nocalo.gdml
