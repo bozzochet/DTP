c20bfc6404aa7ad98cc40c76b4c592688ee48ff6
userlucio <lucio.moriconi@outlook.com>
2020-05-08 17:34:52 +0200

    mostra_eventi.sh improved and updated

--- mostra_eventi.sh
+++ mostra_eventi.sh
@@ -1,3 +1,5 @@
 echo "LD_LIBRARY_PATH=$LD_LIBRARY_PATH"
 
-GGSLeonard -G src/TestGeometry.gdml -i test.root
+GGSWolowitz -g src/libTestGeometry.so -t vgm -o src/TestGeometry.vgm.root
+#GGSLeonard -g src/TestGeometry.gdml -i test.root
+GGSLeonard -g src/TestGeometry.vgm.root -i test.root
