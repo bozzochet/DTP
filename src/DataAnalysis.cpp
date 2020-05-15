35b85422f3f78090f823c4b5c197587a8d5e9af4
userlucio <lucio.moriconi@outlook.com>
2020-05-15 12:14:18 +0200

    ./src/DataAnalysis.cpp

--- src/DataAnalysis.cpp
+++ src/DataAnalysis.cpp
@@ -15,8 +15,7 @@ using namespace std;
 
 int main(int argc, char **argv) {
 
-	const int nLayer = 10;
-
+	const int nLayer = 20;
 	auto inFile = TFile::Open(argv[1]);
 
 	TTree *events;
@@ -33,82 +32,104 @@ int main(int argc, char **argv) {
 	TH1F *h4 = new TH1F("bt_sim", "bt_sim", 1000, 0, 2);
 	TH1F *h5 = new TH1F("btls_sim", "btls_sim", 1500, 0, 15);
 	TH1F *h5cut = new TH1F("btls_sim_cut", "btls_sim_cut", 1500, 0, 15);
-	TH1F *h5nopri = new TH1F("btls_sim_nopri", "btls_sim_nopri", 1500, 0, 15);
+	TH1F *h5nopri = new TH1F("btls_sim_nopri", "btls_sim_nopri", 1500, 0, 15); 
 	TH1F *h5nomip = new TH1F("btls_sim_nomip", "btls_sim_nomip", 1500, 0, 15);
 	TH1F *hPrimEdep = new TH1F("PrimaryEdep", "edep", 500, 0, 0.001);
 	TH1F *hEdep = new TH1F("Edep", "edep", 500, 0, 0.001);
+	TH1F *hprotons = new TH1F("protoni", "protoni", 1000, 0, 4);
+	TH1F *hantip = new TH1F("antiprotoni", "antiprotoni", 1000, 0, 4);
+	TH1F *hneutrons = new TH1F("neutroni", "neutroni", 1000, 0, 4);
+	TH1F *hgamma = new TH1F("fotoni", "gamma", 1000, 0, 4);
+	TH1F *hisotopes = new TH1F("isotopo", "isotopo", 1000, 0, 4);
+	TH1F *helectron = new TH1F("elettroni", "elettroni", 1000, 0, 4);
+	TH1F *hpositron = new TH1F("positroni", "positroni", 1000, 0, 4);
+	TH1F *helectronmu = new TH1F("muoni", "muoni", 1000, 0, 4);
+	TH1F *hpi = new TH1F("pi", "pi", 1000, 0, 4);
+	TH1F *hk = new TH1F("k", "kaoni", 1000, 0, 4);
 	TRandom3 *tr = new TRandom3();
 	vector<vector<TrCluster>> v;
 	
 	for (int i = 0; i < events->GetEntries(); i++) {
-	  v.resize(nLayer);
+
+		v.resize(nLayer);
+		
+	  	//cout << " ------------ " << endl;
+	  	//cout << "Event " << i << endl;
+		
+		events->GetEntry(i);
+		if (a->GetEntries()>10) {
+			printf("Event %d: %d hits\n", i, a->GetEntries());
+	   		for (int j = 0; j < a->GetEntries(); j++) {
+				TrCluster *cl = (TrCluster *)a->At(j);
+				printf("%d) %d %f\n", j, cl->parID, cl->eDep);
+				}
+			}
+		for (int j = 0; j < a->GetEntries(); j++) {
+			TrCluster *cl = (TrCluster *)a->At(j);
+			v[cl->layer].push_back(*cl);
+			if (cl->parID == 0) hPrimEdep->Fill(cl->eDep); //primario
+			if(cl->eDep > 9e-6) hEdep->Fill(cl->eDep); //totale
+			}
+		}
+	// Analisi
 	  
-	  //		cout << " ------------ " << endl;
-	  //		cout << "Event " << i << endl;
+	float tStart = v[0][0].time;
+	float tMean = 0;
+	int _n = 0;
+	for (auto il : v) {
+		for (auto hit : il) {
+			if(tStart>hit.time) tStart = hit.time;
+			if (hit.parID != 0) continue;
+			tMean += hit.time;
+			_n++;
+			}
+		}
+	tMean /= _n;
 	  
-	  events->GetEntry(i);
-	  if (a->GetEntries()>10) {
-	    printf("Event %d: %d hits\n", i, a->GetEntries());
-	    for (int j = 0; j < a->GetEntries(); j++) {
-	      TrCluster *cl = (TrCluster *)a->At(j);
-	      printf("%d) %d %f\n", j, cl->parID, cl->eDep);
-	    }
-	  }
-	  for (int j = 0; j < a->GetEntries(); j++) {
-	    TrCluster *cl = (TrCluster *)a->At(j);
-	    v[cl->layer].push_back(*cl);
-	    if (cl->parID == 0) { // primario
-	      hPrimEdep->Fill(cl->eDep);
-	    }
-	    hEdep->Fill(cl->eDep);
-	  }
+	for (int il = 0; il<v.size(); il++) {
+		for (int hit = 0; hit<v[il].size(); hit++) {
+			//if(1) (
+			//v[9].size()>5&&v[il][hit].eDep>0.00001 
+			//10 keV 
+			//cout<<v[il][hit].parPdg <<endl;
+			
+			//eDep
+			if (v[il][hit].parPdg == 2212)
+				hprotons->Fill(log10(v[il][hit].time - tStart + 1));
+			if (v[il][hit].parPdg == -2212)
+				hantip->Fill(log10(v[il][hit].time - tStart + 1));
+			if (v[il][hit].parPdg == 2112)
+				hneutrons->Fill(log10(v[il][hit].time - tStart + 1));
+			if (v[il][hit].parPdg == 11) {
+				helectron->Fill(log10(v[il][hit].time - tStart + 1));
+			if (v[il][hit].parPdg == 211 || v[il][hit].parPdg == -211)
+				hpi->Fill(log10(v[il][hit].time - tStart + 1));
+			if (v[il][hit].parPdg == 130 || v[il][hit].parPdg == 310 || v[il][hit].parPdg == 311 || v[il][hit].parPdg == 321 || v[il][hit].parPdg == -321)
+				hk->Fill(log10(v[il][hit].time - tStart + 1));
+			if (v[il][hit].parPdg == 13 || v[il][hit].parPdg == -13)
+				helectronmu->Fill(log10(v[il][hit].time - tStart + 1));
+			if (v[il][hit].parPdg == 22)
+				hgamma->Fill(log10(v[il][hit].time - tStart + 1));
+			if (v[il][hit].parPdg > 1000000000)
+				hisotopes->Fill(log10(v[il][hit].time - tStart + 1));
+			
+			//time
+			double smearedtime = tr->Gaus(v[il][hit].time - tStart,0.1);
+			h1->Fill(v[il][hit].time - tStart);
+			h2->Fill(v[il][hit].time - tStart);
+			h3->Fill(log10(v[il][hit].time - tStart + 1));
+			h4->Fill(smearedtime);
+			h5->Fill(smearedtime);
+			
+			if (smearedtime<0.55) h5cut->Fill(smearedtime);
+			if (v[il][hit].parID != 0) h5nopri->Fill(smearedtime);
+			if (v[9].size()>5) h5nomip->Fill(smearedtime);
+			if (v[il][hit].parID == 0) h->Fill(v[il][hit].time - tMean); //ns
+			}
+		}
 	  
-	  // Analisi
-	  
-	  float tStart = v[0][0].time;
-	  float tMean = 0;
-	  int _n = 0;
-	  for (auto il : v) {
-	    for (auto hit : il) {
-	      if(tStart>hit.time) tStart = hit.time;
-	      if (hit.parID != 0) continue;
-	      tMean += hit.time;
-	      _n++;
-	    }
-	  }
-	  tMean /= _n;
-	  //		cout<<"tempo minimo "<<tStart<<endl;
-	  
-	  for (int il = 0; il<v.size(); il++) {
-	    for (int hit = 0; hit<v[il].size(); hit++) {
-	      if(
-		 1 &&
-		 //					v[9].size()>5 &&
-		 v[il][hit].eDep>0.00001 //10 keV
-		 ){
-		//					cout<<	v[il][hit].parPdg <<endl;
-		h1->Fill(v[il][hit].time - tStart);
-		h2->Fill(v[il][hit].time - tStart);
-		double smearedtime = tr->Gaus(v[il][hit].time - tStart,0.1);
-		h3->Fill(log10(v[il][hit].time - tStart + 1));
-		h4->Fill(smearedtime);
-		h5->Fill(smearedtime);
-		if (smearedtime<0.55) h5cut->Fill(smearedtime);
-		if (v[il][hit].parID != 0) h5nopri->Fill(smearedtime);
-		if (v[9].size()>5) h5nomip->Fill(smearedtime);
-	      }
-	      if (v[il][hit].parID == 0) {
-		//				cout << v[il][hit].time - tMean << endl;
-		h->Fill(v[il][hit].time - tMean); //ns
-	      }
-	    }
-	  }
-	  
-	  //		std::cout << " ------------ " << std::endl;
-	  
-	  v.clear();
-	}
-	
+	v.clear();
+
 	outFile->WriteTObject(h);
 	outFile->WriteTObject(h1);
 	outFile->WriteTObject(h2);
@@ -120,5 +141,15 @@ int main(int argc, char **argv) {
 	outFile->WriteTObject(h5nomip);
 	outFile->WriteTObject(hPrimEdep);
 	outFile->WriteTObject(hEdep);
+	outFile->WriteTObject(hprotons);
+	outFile->WriteTObject(hneutrons);
+	outFile->WriteTObject(hgamma);
+	outFile->WriteTObject(hisotopes);
+	outFile->WriteTObject(helectron);
+	outFile->WriteTObject(hpositron);
+	outFile->WriteTObject(helectronmu);
+	outFile->WriteTObject(hpi);
+	outFile->WriteTObject(hk);
 	outFile->Close();
+	}
 }
