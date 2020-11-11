d10bb466ce60ae6b0390065519a96c16f11d68e0
cav-fiumella <67470739+cav-fiumella@users.noreply.github.com>
2020-11-11 01:05:38 +0100

    minor

--- src/TimeAnalysis.cpp
+++ src/TimeAnalysis.cpp
@@ -78,7 +78,7 @@ int main(int argc, char **argv)
   TH1F *h_time = new TH1F
   (
     "h_time", "time measures; t_meas - t_true [s]; entries",
-    10000, -4e-10, 0
+    10000, -5e-11, 5e-11
   );
 
   TRandom3 *random = new TRandom3(9298);
