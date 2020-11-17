96a9c3d83917c56370eec23d535b95fd59b154d5
cav-fiumella <67470739+cav-fiumella@users.noreply.github.com>
2020-11-17 09:33:13 +0100

    minor: geo datacard in useful_commands scripts

--- useful_commands/run.sh
+++ useful_commands/run.sh
@@ -2,5 +2,5 @@
 
 #echo "LD_LIBRARY_PATH=$LD_LIBRARY_PATH"
 
-GGSPenny -g lib/libTestGeometry.so -d macros/run.mac -ro test.root
-GGSPenny -g lib/libTestGeometry_nocalo.so -d macros/run.mac -ro test_nocalo.root
+GGSPenny -g lib/libTestGeometry.so -gd macros/geo.mac -d macros/run.mac -ro test.root
+GGSPenny -g lib/libTestGeometry_nocalo.so -gd macros/geo.mac -d macros/run.mac -ro test_nocalo.root
