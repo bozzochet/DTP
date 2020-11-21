bfca16e17f37570c0476caca7c224ce5a6da9aa7
cav-fiumella <67470739+cav-fiumella@users.noreply.github.com>
2020-11-21 18:30:45 +0100

    WORK: minor

--- useful_commands/run_e-p.sh
+++ useful_commands/run_e-p.sh
@@ -12,9 +12,10 @@ echo 10 GeV && \
 echo && \
 
 #GGSPenny -g lib/libTestGeometry.so -gd macros/geo.mac -d macros/e_10.mac -ro simulations/e/10/test.root && \
-exe/Digitization simulations/e/10/test.root simulations/e/10/digit.root && \
-exe/DataAnalysis simulations/e/10/digit.root simulations/e/10/histos.root && \
+#exe/Digitization simulations/e/10/test.root simulations/e/10/digit.root && \
+#exe/DataAnalysis simulations/e/10/digit.root simulations/e/10/histos.root && \
 
+echo && \
 echo 100 GeV && \
 echo && \
 
@@ -22,6 +23,7 @@ GGSPenny -g lib/libTestGeometry.so -gd macros/geo.mac -d macros/e_100.mac -ro si
 exe/Digitization simulations/e/100/test.root simulations/e/100/digit.root && \
 exe/DataAnalysis simulations/e/100/digit.root simulations/e/100/histos.root && \
 
+echo && \
 echo 1 TeV && \
 echo && \
 
@@ -29,6 +31,7 @@ GGSPenny -g lib/libTestGeometry.so -gd macros/geo.mac -d macros/e_1000.mac -ro s
 exe/Digitization simulations/e/1000/test.root simulations/e/1000/digit.root && \
 exe/DataAnalysis simulations/e/1000/digit.root simulations/e/1000/histos.root && \
 
+echo && \
 echo 10 TeV && \
 echo && \
 
@@ -37,7 +40,8 @@ echo && \
 #exe/DataAnalysis simulations/e/10000/digit.root simulations/e/10000/histos.root && \
 
 
-echo electrons: && \
+echo && \
+echo protons: && \
 echo && \
 
 echo 10 GeV && \
@@ -47,6 +51,7 @@ GGSPenny -g lib/libTestGeometry.so -gd macros/geo.mac -d macros/e_10.mac -ro sim
 exe/Digitization simulations/p/10/test.root simulations/p/10/digit.root && \
 exe/DataAnalysis simulations/p/10/digit.root simulations/p/10/histos.root && \
 
+echo && \
 echo 100 GeV && \
 echo && \
 
@@ -54,6 +59,7 @@ GGSPenny -g lib/libTestGeometry.so -gd macros/geo.mac -d macros/e_100.mac -ro si
 exe/Digitization simulations/p/100/test.root simulations/p/100/digit.root && \
 exe/DataAnalysis simulations/p/100/digit.root simulations/p/100/histos.root && \
 
+echo && \
 echo 1 TeV && \
 echo && \
 
@@ -61,6 +67,7 @@ GGSPenny -g lib/libTestGeometry.so -gd macros/geo.mac -d macros/e_1000.mac -ro s
 exe/Digitization simulations/p/1000/test.root simulations/p/1000/digit.root && \
 exe/DataAnalysis simulations/p/1000/digit.root simulations/p/1000/histos.root && \
 
+echo && \
 echo 10 TeV && \
 echo && \
 
